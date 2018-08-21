60d9db306cb8d037290fde66064980c3099e75a1
github username <github email address>
2018-08-21 17:20:11 -0500

    Created all migrations and models

---
+++ db/migrate/20180821221230_create_figure_titles.rb
@@ -0,0 +1,8 @@
+class CreateFigureTitles < ActiveRecord::Migration
+  def change
+    create_join_table :figures, :titles do |t|
+      t.index :figure_id
+      t.index :title_id
+    end
+  end
+end
